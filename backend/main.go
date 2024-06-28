776dacb1e3ffaf1477237a0a3fe1e2ed92bf546b
sentixxx <1091441107@qq.com>
2024-06-28 15:49:48 +0800

    feat(backend): :boom: move to beego

--- backend/main.go
+++ backend/main.go
@@ -1,81 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"time"
+	_ "backend/routers"
 
-	"backend/models"
-	"backend/utils"
-
-	"github.com/gin-gonic/gin"
-	_ "github.com/go-sql-driver/mysql"
+	"github.com/astaxie/beego"
 )
 
-
-
-func initGin() {
-	r := gin.Default()
-	r.POST("/register",func(c *gin.Context){
-		var u models.User
-		err := c.BindJSON(&u)
-		if err != nil {
-			c.JSON(http.StatusBadRequest,gin.H{"status": "failed"})
-			return
-		}
-		c.JSON(http.StatusOK,gin.H{
-			"username": u.Username,
-			"password": u.Password,})
-		u.Mail = "default"
-		u.Status = 1
-		_, err = models.InsertUser(u)
-		if err != nil {
-			fmt.Println("insert user failed")
-			c.JSON(http.StatusBadRequest,gin.H{"status": "failed"})
-			return
-		}
-		fmt.Println("register success")
-	})
-
-	r.POST("/login",func(c *gin.Context){
-		var u models.User
-		err := c.BindJSON(&u)
-		if err != nil {
-			c.JSON(http.StatusBadRequest,gin.H{"status": "failed"})
-			return
-		}
-		id := models.QueryUserIdWithUsername(u.Username)
-		fmt.Println("id:",id)
-		if id == 0 {
-			c.JSON(http.StatusBadRequest,gin.H{"status": "failed"})
-			return
-		}
-		password := models.QueryUserPasswordWithId(id)
-		fmt.Println("password:",password)
-		if password != u.Password {
-			c.JSON(http.StatusBadRequest,gin.H{"status": "failed"})
-			return
-		}
-		c.JSON(http.StatusOK,gin.H{"status": "success"})
-	});
-
-
-
-	r.Run(":8090")
-}
-
-
-func main()  {
-	err := utils.InitDB()
-	if err != nil {
-		fmt.Printf("initDB failed, err:%v\n", err)
-		return
+func main() {
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
 	}
-	fmt.Println("connect to db success")
-	initGin()
-	go utils.KeepAlive(1*time.Minute)
-	select{
-
-
-	}
+	beego.Run()
 }
